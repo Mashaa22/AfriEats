9c889dbe937d5cee9a3cadd7453eb78b7a06f497
Omari99 <mariam.ali01@student.moringaschool.com>
2023-04-18 20:20:09 +0300

    added meals componet

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Meals from './components/Meals';
 // import Restaurant from './components/Restaurant';
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
@@ -14,7 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="home" />} />
           <Route path="/home" element={<Home />} />
-          
+          <Route path="/meals" element={<Meals />} />
+
+
           
           
           
