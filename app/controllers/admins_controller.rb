7d82343b1c2520a3c206b95f3207161dbe07d4ea
AlexMachariaMaranga <amaranga22@gmail.com>
2023-05-05 16:47:57 +0300

    uncomment sendgrid to see deploy

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -13,23 +13,23 @@ class AdminsController < ApplicationController
     render json: @admin
   end
 
-  # POST /admins
-  # def create
-  #   @admin = Admin.new(admin_params)
+  POST /admins
+  def create
+    @admin = Admin.new(admin_params)
 
-  #   if @admin.save
-  #     # Generate and save a random 6-digit PIN for the admin
-  #     pin = rand(100_000..999_999)
-  #     @admin.update(pin: pin)
+    if @admin.save
+      # Generate and save a random 6-digit PIN for the admin
+      pin = rand(100_000..999_999)
+      @admin.update(pin: pin)
 
-  #     # Send an email to the admin with the PIN
-  #     AdminMailer.send_pin(@admin, pin).deliver
+      # Send an email to the admin with the PIN
+      AdminMailer.send_pin(@admin, pin).deliver
 
-  #     render json: @admin, status: :created, location: @admin
-  #   else
-  #     render json: @admin.errors, status: :unprocessable_entity
-  #   end
-  # end
+      render json: @admin, status: :created, location: @admin
+    else
+      render json: @admin.errors, status: :unprocessable_entity
+    end
+  end
 
   # PATCH/PUT /admins/1
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
 
 
+
   post '/user/signup', to: 'users#create'
   post '/admin/signup', to: 'admins#create'
   post '/login', to: 'auths#create'
@@ -14,8 +15,6 @@ Rails.application.routes.draw do
   get '/logged_in', to: 'application#logged_in?'
   post '/auto_login', to: 'auths#auto_login'
   get '/auto_login', to: 'auths#auto_login'
-
   get '/admin_orders', to: 'orders#admin_orders'
-
 
 end
